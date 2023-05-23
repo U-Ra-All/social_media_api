9b59465d10c098484bc73726018d5fb42cb7d5c9
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-23 16:55:11 +0300

    Add more fields to  Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -9,16 +9,24 @@ class Profile(models.Model):
         female = 2
         other = 3
 
+    created_at = models.DateTimeField(auto_now_add=True)
+
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
+
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
     gender = models.PositiveSmallIntegerField(
         choices=Gender.choices, default=Gender.male
     )
 
     birth_date = models.DateField(null=True, blank=True, default=None)
-
     phone = models.CharField(max_length=20, null=True, blank=True)
 
     follows = models.ManyToManyField(
@@ -27,3 +35,6 @@ class Profile(models.Model):
         symmetrical=False,
         blank=True,
     )
+
+    def __str__(self):
+        return self.full_name
