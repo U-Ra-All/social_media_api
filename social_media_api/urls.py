fd1e42f9f996cccd02e0e616f862b6d87de573bd
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-23 09:49:20 +0300

    Add api/profiles/ to app urls

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/profiles/", include("profiles.urls", namespace="profiles")),
 ]
