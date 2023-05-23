4c8e463176edba9cbcffd48629750c9f2b17731e
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-23 09:48:00 +0300

    Create Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,15 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        symmetrical=False,
+        blank=True,
+    )
