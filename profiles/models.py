6447694d95262363c2a122437b3d1de46e285f0d
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-23 11:35:45 +0300

    Change Profile model and register it in admin.py

--- profiles/models.py
+++ profiles/models.py
@@ -4,9 +4,23 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
+    class Gender(models.IntegerChoices):
+        male = 1
+        female = 2
+        other = 3
+
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
+
+    gender = models.PositiveSmallIntegerField(
+        choices=Gender.choices, default=Gender.male
+    )
+
+    birth_date = models.DateField(null=True, blank=True, default=None)
+
+    phone = models.CharField(max_length=20, null=True, blank=True)
+
     follows = models.ManyToManyField(
         "self",
         related_name="followed_by",
