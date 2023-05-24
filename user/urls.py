51c3a6a6ec5502e2cc354f96ccae60eb02098288
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-24 14:21:07 +0300

    Implement logout

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from user.views import CreateUserView, ManageUserView
+from user.views import CreateUserView, ManageUserView, LogoutView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
@@ -13,6 +13,7 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("logout/", LogoutView.as_view(), name="auth_logout"),
 ]
 
 app_name = "user"
