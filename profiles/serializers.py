226b9525a1c8ecd667b5829f90d3eeb7c6033ae4
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-24 10:06:13 +0300

    Add more fields to ProfileSerializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -6,4 +6,14 @@ from profiles.models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ("id", "user", "follows")
+        fields = (
+            "id",
+            "created_at",
+            "user",
+            "first_name",
+            "last_name",
+            "gender",
+            "birth_date",
+            "phone",
+            "follows",
+        )
