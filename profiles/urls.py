63d605fd63630b07927bf7eea9273abe07c009f5
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-26 08:41:20 +0300

    Add me/ path in profiles/urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,10 +1,13 @@
+from django.urls import path, include
 from rest_framework import routers
 
-from profiles.views import ProfileViewSet
+from profiles.views import ProfileViewSet, MyProfileViewSet
 
 router = routers.DefaultRouter()
 router.register("", ProfileViewSet)
 
-urlpatterns = router.urls
-
+urlpatterns = [
+    path("me/", MyProfileViewSet.as_view()),
+    path("", include(router.urls)),
+]
 app_name = "profiles"
