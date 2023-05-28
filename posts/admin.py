7c0b755f6a48d607df0542df2e321fe0d012e6c3
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-28 09:23:56 +0300

    Register Like model in admin.py

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from posts.models import Post
+from posts.models import Post, Like
 
 admin.site.unregister(Group)
 admin.site.register(Post)
+admin.site.register(Like)
