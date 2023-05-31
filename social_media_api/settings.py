da9bf9f0a3465a240a2fca0cf48a1e70a69ac108
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-05-31 11:49:29 +0300

    Change path to media folder

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # Actual directory user files go to
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = BASE_DIR / "media"
 
 # URL used to access the media
 MEDIA_URL = "/media/"
