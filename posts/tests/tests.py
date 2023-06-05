47e5874b9cd0f7e1c9e2d906efa2533ab2f3b585
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-06-05 16:53:15 +0300

    Implement PostModelTest

--- posts/tests/tests.py
+++ posts/tests/tests.py
@@ -71,3 +71,60 @@ class AuthenticatedPostApiTest(TestCase):
 
         for key in payload:
             self.assertEqual(payload[key], getattr(post, key))
+
+
+class PostModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = get_user_model().objects.create_user(
+            email="test_user@mail.com",
+            password="test_password",
+        )
+        profile = Profile.objects.create(
+            user=user,
+            first_name="Sample first_name",
+            last_name="Sample last_name",
+            gender=1,
+            birth_date="2023-05-31",
+            phone="1111111111",
+        )
+        Post.objects.create(
+            title="Sample title 1",
+            body="Sample body 1",
+            user_profile=profile,
+        )
+
+    def test_created_at_label(self):
+        post = Post.objects.get(id=1)
+        field_label = post._meta.get_field("created_at").verbose_name
+        self.assertEqual(field_label, "created at")
+
+    def test_title_label(self):
+        post = Post.objects.get(id=1)
+        field_label = post._meta.get_field("title").verbose_name
+        self.assertEqual(field_label, "title")
+
+    def test_body_label(self):
+        post = Post.objects.get(id=1)
+        field_label = post._meta.get_field("body").verbose_name
+        self.assertEqual(field_label, "body")
+
+    def test_image_label(self):
+        post = Post.objects.get(id=1)
+        field_label = post._meta.get_field("image").verbose_name
+        self.assertEqual(field_label, "image")
+
+    def test_user_profile_label(self):
+        post = Post.objects.get(id=1)
+        field_label = post._meta.get_field("user_profile").verbose_name
+        self.assertEqual(field_label, "user profile")
+
+    def test_title_max_length(self):
+        post = Post.objects.get(id=1)
+        max_length = post._meta.get_field("title").max_length
+        self.assertEqual(max_length, 255)
+
+    def test_post_str(self):
+        post = Post.objects.get(id=1)
+        expected_object_name = f"{post.title}"
+        self.assertEqual(str(post), expected_object_name)
